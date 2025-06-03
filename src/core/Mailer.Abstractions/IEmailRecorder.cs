0dddc71a4c47307c5f5ae7840b1ff990237905ce
Philip Ochu <pochu@eminenttechnology.com>
2025-06-02 19:40:30 -0500

    commit

--- src/core/Mailer.Abstractions/IEmailRecorder.cs
+++ src/core/Mailer.Abstractions/IEmailRecorder.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Mailer.Abstractions
 {
     public interface IEmailRecorder
     {
-       
-
         Task RecordSuccess(EmailMessage message);
-
         Task RecordFailure(EmailMessage message, Exception error);
     }
 }
--- src/recorders/Mailer.Recorders.Sql/SqlRecorder.cs
+++ src/recorders/Mailer.Recorders.Sql/SqlRecorder.cs
@@ -3,11 +3,7 @@ using Mailer.Abstractions;
 using Mailer.Sql;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Mailer.Recorders.Sql
